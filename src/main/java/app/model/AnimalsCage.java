b0e92535d7e26b4cbf02d355bf4781fa38e7c206
Strelkatlet <ignatievwa@gmail.com>
2022-07-28 18:04:30 +0300

    Решение 2.1.3

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,12 +1,15 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-
     @Autowired
+    private Timer timer;
+    @Autowired
+    @Qualifier("dogBean")
     private Animal animal;
 
     public void whatAnimalSay() {
@@ -15,5 +18,9 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
